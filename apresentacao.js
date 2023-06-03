1f00e91660b7c7cb23430bf46410363fa7fd4cb4
leonelsmp <72762001+leonelsmp@users.noreply.github.com>
2023-06-03 18:59:16 +0000

    Commit 10 - Criando Arquivos

---
+++ apresentacao.js
@@ -0,0 +1,12 @@
+var formatarMoeda = require("./util.js");
+
+//função principal
+module.exports = function gerarFaturaStr (fatura, calc) {
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+    for (let apre of fatura.apresentacoes) {
+      faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    }
+    faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura))}\n`;
+    faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura)} \n`;
+    return faturaStr;
+  }
